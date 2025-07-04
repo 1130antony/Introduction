ab6cd861d0b613da65039a3c5967ae7387c8dccb
1130antony <99240802+1130antony@users.noreply.github.com>
2025-07-04 10:49:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function switchLanguage(lang) {
   document.querySelectorAll('.lang.zh').forEach(el => {
-    el.style.display = lang === 'zh' ? 'inline' : 'none';
+    el.style.display = (lang === 'zh') ? 'block' : 'none';
   });
   document.querySelectorAll('.lang.en').forEach(el => {
-    el.style.display = lang === 'en' ? 'inline' : 'none';
+    el.style.display = (lang === 'en') ? 'block' : 'none';
   });
 }
